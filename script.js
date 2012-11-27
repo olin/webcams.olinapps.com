d428f493bb03b6313f638615df86e314a08d3c04
Tim Cameron Ryan <id@timryan.org>
2012-11-27 11:47:27 -0500

    Antagonize everybody

---
+++ script.js
@@ -0,0 +1,6 @@
+// Include jQuery.
+document.write("<script src='http://code.jquery.com/jquery.min.js'></script>");
+// Include Bootstrap JavaScript add-ons.
+document.write("<script src='http://netdna.bootstrapcdn.com/twitter-bootstrap/2.2.1/js/bootstrap.min.js'></script>");
+
+/*** ADD YOUR CODE BELOW ***/
